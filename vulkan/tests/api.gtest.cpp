148dc42a4dc157d1af113aaeac275567a08d357c
aldhinn <aldespinas@gmail.com>
2024-05-26 13:41:38 -0600

    removed unnecessary comments.

--- vulkan/tests/api.gtest.cpp
+++ vulkan/tests/api.gtest.cpp
@@ -68,39 +68,6 @@ namespace celerique { namespace vulkan {
     }
 
     TEST_F(GraphicsApiUnitTestCpp, expectedBehaviourWhenCallingTwice) {
-/*
-IN DEBUG MODE, these log messages are expected to appear:
-
-[DEBUG] Created an x11 window. x11/src/window.cpp:92
-[TRACE] Registered an x11 vulkan surface. vulkan/src/manager.cpp:437
-[TRACE] Selected the best physical device for graphics. vulkan/src/manager.cpp:522
-[TRACE] An existing graphics logical device of a window with the same UI protocol is to be used. vulkan/src/manager.cpp:89
-[TRACE] Present mailbox mode. vulkan/src/manager.cpp:878
-[TRACE] Created swapchain. vulkan/src/manager.cpp:705
-[TRACE] Created swapchain image views. vulkan/src/manager.cpp:775
-[TRACE] Using existing render pass for a previously registered window of the same UI protocol. vulkan/src/manager.cpp:782
-[DEBUG] Registered window. vulkan/src/manager.cpp:96
-[TRACE] Destroyed window image views. vulkan/src/manager.cpp:129
-[TRACE] Erased window swapchain extent. vulkan/src/manager.cpp:133
-[TRACE] Destroyed window swapchain. vulkan/src/manager.cpp:140
-[TRACE] Destroyed window surface. vulkan/src/manager.cpp:146
-[DEBUG] Removed window from registry. vulkan/src/manager.cpp:150
-[TRACE] Registered an x11 vulkan surface. vulkan/src/manager.cpp:437
-[TRACE] Selected the best physical device for graphics. vulkan/src/manager.cpp:522
-[TRACE] An existing graphics logical device of a window with the same UI protocol is to be used. vulkan/src/manager.cpp:89
-[TRACE] Present mailbox mode. vulkan/src/manager.cpp:878
-[TRACE] Created swapchain. vulkan/src/manager.cpp:705
-[TRACE] Created swapchain image views. vulkan/src/manager.cpp:775
-[TRACE] Using existing render pass for a previously registered window of the same UI protocol. vulkan/src/manager.cpp:782
-[DEBUG] Registered window. vulkan/src/manager.cpp:96
-[DEBUG] X11 window destroyed. x11/src/window.cpp:529
-[TRACE] Destroyed window image views. vulkan/src/manager.cpp:129
-[TRACE] Erased window swapchain extent. vulkan/src/manager.cpp:133
-[TRACE] Destroyed window swapchain. vulkan/src/manager.cpp:140
-[TRACE] Destroyed window surface. vulkan/src/manager.cpp:146
-[DEBUG] Removed window from registry. vulkan/src/manager.cpp:150
-
-*/
         ::std::unique_ptr<IWindow> ptrWindow = createWindow(
             700, 300, ""
         );
