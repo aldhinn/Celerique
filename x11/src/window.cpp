2898681a59c4ed5a259558e9cec570122c43d4d5
aldhinn <aldespinas@gmail.com>
2024-06-25 20:55:54 -0600

    recreate swapchain on a separate thread.

--- x11/src/window.cpp
+++ x11/src/window.cpp
@@ -141,12 +141,15 @@ void ::celerique::x11::internal::Window::onUpdate(::std::shared_ptr<IUpdateData>
             _atomicRecentWindowWidth.store(width);
             _atomicRecentWindowHeight.store(height);
 
-            /// @brief The retrieved shared pointer of this window's graphics API interface.
-            ::std::shared_ptr<IGraphicsAPI> ptrGraphicsApi = _weakPtrGraphicsApi.lock();
-            // Check if not null before re-creating swapchain.
-            if (ptrGraphicsApi != nullptr) {
-                ptrGraphicsApi->recreateSwapChain(_windowHandle);
-            }
+            ::std::thread recreateSwapChainThread([&]() {
+                /// @brief The retrieved shared pointer of this window's graphics API interface.
+                ::std::shared_ptr<IGraphicsAPI> ptrGraphicsApi = _weakPtrGraphicsApi.lock();
+                // Check if not null before re-creating swapchain.
+                if (ptrGraphicsApi != nullptr) {
+                    ptrGraphicsApi->recreateSwapChain(_windowHandle);
+                }
+            });
+            recreateSwapChainThread.detach();
         }
     } return;
 
